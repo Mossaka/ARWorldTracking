a231288fc3df3fb978bc8e4d460faa6c31184049
Jiaxiao Zhou <jiz417@ucsd.edu>
2017-10-14 23:21:09 -0700

    added the project and enabled the AR debug options

---
+++ AR World Tracking/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  AR World Tracking
+//
+//  Created by Jiaxiao Zhou on 10/14/17.
+//  Copyright © 2017 mossaka. All rights reserved.
+//
+
+import UIKit
+import ARKit
+class ViewController: UIViewController {
+
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        self.sceneView.session.run(configuration)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
